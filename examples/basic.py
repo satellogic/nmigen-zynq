37950cb787081aebe2b5105ab2a9f10f42ecce5c
Andres Demski <andresdemski@gmail.com>
2020-06-03 15:21:43 -0300

    examples/basic.py: basic example

---
+++ examples/basic.py
@@ -0,0 +1,37 @@
+from nmigen import *
+from nmigen.lib.cdc import ResetSynchronizer
+from nmigen_zynq.ps import PsZynqMP
+from nmigen_zynq.plat import ZynqMPPlatform
+
+
+class Zu3egPlatform(ZynqMPPlatform):
+    device     = 'xczu3eg'
+    package    = 'sfva625'
+    speed      = '1'
+    grade      = 'e'
+    resources  = []
+    connectors = []
+
+
+class BasicExample(Elaboratable):
+    def elaborate(self, platform):
+        m = Module()
+        m.domains += ClockDomain('sync')
+        m.submodules.ps = ps = PsZynqMP()
+
+        cnt = Signal(29)
+        m.d.sync += cnt.eq(cnt + 1)
+        m.d.comb += ps.get_irq_signal(0).eq(cnt[-1])
+
+        clk = ps.get_clock_signal(0, 200e6)
+        m.d.comb += ClockSignal('sync').eq(clk)
+
+        reset = ps.get_reset_signal(0)
+        reset_sync = ResetSynchronizer(reset, domain='sync')
+        m.submodules.reset_sync = reset_sync
+        return m
+
+
+core = BasicExample()
+plat = Zu3egPlatform()
+plat.build(core)
