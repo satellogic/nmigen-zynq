89c0c87801f3a6d5f2059b9af88c39231e24d41d
Andres Demski <andresdemski@gmail.com>
2020-06-24 15:38:11 -0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+def scm_version():
+    def local_scheme(version):
+        if version.tag and not version.distance:
+            return version.format_with("")
+        else:
+            return version.format_choice("+{node}", "+{node}.dirty")
+    return {
+        "relative_to": __file__,
+        "version_scheme": "guess-next-dev",
+        "local_scheme": local_scheme
+    }
+
+
+setup(
+    name="nmigen_zynq",
+    use_scm_version=scm_version(),
+    author="andresdemski",
+    author_email="andresdemski@gmail.com",
+    description="nMigen Zynq support",
+    license="BSD",
+    python_requires="~=3.6",
+    install_requires=[
+        "nmigen",
+    ],
+    packages=find_packages(exclude=["*.test*"]),
+)
